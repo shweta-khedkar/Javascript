00644839beafef846774a743a7a6733251e3d4ff
shweta khedkar <137026859+shweta-khedkar@users.noreply.github.com>
2024-03-19 09:36:26 +0000

    Conversion done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,9 @@
+let val=20;
+console.log(typeof(val));
+console.log(val);
+let convert=String(val);
+console.log(typeof convert);
+console.log(convert);
+let val1=3;
+let convertboolean=Boolean(val1);
+console.log(convertboolean);
